bd23721eacdecf439aff7b5b22820735f2df9c3c
Iván Gabriel <tocutoeltuco@gmail.com>
2022-01-04 13:59:45 -0300

    Support new x_bibliotheques format

--- fetch-assets.sh
+++ fetch-assets.sh
@@ -1,18 +1,17 @@
 swfs=("costume" "x_meli_costumes" "x_fourrures" "x_items_chaman")
-link="http://www.transformice.com/images/x_bibliotheques/"
 
-printf "Downloading swfs\n"
-mkdir ./swfs
+download_incremental () {
+	local link="http://www.transformice.com/images/x_bibliotheques/$1"
 
-for swf in "${swfs[@]}"
-do
-	for (( c=1; 1>0; c++))
+	prefix="$2" # link prefix
+	start=$(($3+0)) # cast parameter #2 (start) to int
+	for (( c=$start; 1>0; c++ ))
 	do
-		if [[ $c == "1" && $swf != "costume" ]]
+		if [[ $c == "1" && $4 == "true" ]]
 		then
-			file="$swf.swf"
+			file="$prefix.swf"
 		else
-			file="$swf$c.swf"
+			file="$prefix$c.swf"
 		fi
 
 		response=$(curl --write-out "%{http_code}" --head --silent --output /dev/null "$link$file")
@@ -24,43 +23,121 @@ do
 			curl "$link$file" > "./swfs/$file"
 		fi
 	done
+}
+
+export_sprites () {
+	mkdir ./sprites
+
+	local costume="Costume_[0-9]+_"
+	local fur="_1_[0-9]+_1$"
+	local shaman="Objet_[0-9]+"
+
+	for swf in ./swfs/*
+	do
+		printf "Exporting $(basename -- $swf)\n"
+		ffdec -format sprite:svg -export sprite ./sprites "$swf" > /dev/null
+
+		printf "Cleaning up $(basename -- $swf)\n"
+
+		for file in ./sprites/*
+		do
+			if [[ $file =~ $costume ]]
+			then
+				mv $file "./costumes/$(basename -- $file)"
+
+			elif [[ $file =~ $fur ]]
+			then
+				mv $file "./furs/$(basename -- $file)"
+
+			elif [[ $file =~ $shaman ]]
+			then
+				mv $file "./shaman/$(basename -- $file)"
+
+			else
+				rm -rf $file
+			fi
+		done
+	done
+
+	rm -rf ./swfs/ ./sprites/
+}
+
+mkdir ./swfs
+for swf in "${swfs[@]}"
+do
+	start=1
+	if [[ $swf == "costume" ]]
+	then
+		ignore="false"
+	else
+		ignore="true"
+	fi
+
+	printf "Downloading $swf\n"
+	download_incremental "" "$swf" "$start" "$ignore"
 done
 
 printf "Exporting sprites\n"
-mkdir ./sprites
 mkdir ./furs
 mkdir ./costumes
 mkdir ./shaman
+export_sprites
 
-costume="Costume_[0-9]+_"
-fur="_1_[0-9]+_1$"
-shaman="Objet_[0-9]"
+mkdir ./swfs
+# fur_regex="_1_([0-9]+)_1$"
+# last_fur=0
+# for fur in ./furs/*
+# do
+# 	if [[ $fur =~ $fur_regex ]]
+# 	then
+# 		id="${BASH_REMATCH[1]}"
+# 		if [[ $last_fur -lt $id ]]
+# 		then
+# 			last_fur=$id
+# 		fi
+# 	fi
+# done
 
-for swf in ./swfs/*
+# last_fur=$(($last_fur+1))
+# printf "Downloading rest of furs starting from $last_fur\n"
+# download_incremental "fourrures/" "f" "$last_fur" "false"
+printf "Downloading rest of furs starting from 218\n"
+download_incremental "fourrures/" "f" "218" "false"
+
+shaman_regex="Objet_([0-9]+)"
+declare -A shaman_items
+for item in ./shaman/*
 do
-	printf "Exporting $(basename -- $swf)\n"
-	ffdec -format sprite:svg -export sprite ./sprites "$swf" > /dev/null
-
-	printf "Cleaning up $(basename -- $swf)\n"
-
-	for file in ./sprites/*
-	do
-		if [[ $file =~ $costume ]]
+	if [[ $item =~ $shaman_regex ]]
+	then
+		id=$((${BASH_REMATCH[1]}+0))
+		if [[ $id -gt 9999 ]]
 		then
-			mv $file "./costumes/$(basename -- $file)"
-
-		elif [[ $file =~ $fur ]]
+			base=$(((id-10000)/10000))
+			skin=$(((id-10000)%10000))
+		elif [[ $id -gt 99 ]]
 		then
-			mv $file "./furs/$(basename -- $file)"
-
-		elif [[ $file =~ $shaman ]]
-		then
-			mv $file "./shaman/$(basename -- $file)"
-
+			base=$((id/100))
+			skin=$((id%100))
 		else
-			rm -rf $file
+			base=$id
+			skin=0
 		fi
-	done
+
+		if [[ ${shaman_items[$base]} -lt $skin || $skin == 0 ]]
+		then
+			shaman_items[$base]=$skin
+		fi
+	fi
 done
 
-rm -rf ./swfs/ ./sprites/
+for base in "${!shaman_items[@]}"
+do
+	skin=$((${shaman_items[$base]}+1))
+	printf "Downloading rest of skins for $base starting from $skin\n"
+	download_incremental "chamanes/" "o$base," "$skin" "false"
+done
+
+printf "Exporting sprites\n"
+export_sprites
+
